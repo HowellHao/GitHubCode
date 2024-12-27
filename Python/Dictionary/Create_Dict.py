065e2e60678cbd031a857f04b6344623a95ddf50
HowellHao <howellhao.work@gmail.com>
2024-12-26 23:43:30 -0700

    add files to Python

---
+++ Python/Dictionary/Create_Dict.py
@@ -0,0 +1,8 @@
+my_dict={}
+my_dict['name']='Jack'
+my_dict['age']=26
+my_dict['address']='Downtown'
+my_dict['phone']=1234567890
+my_dict_2={'name':'Jill','age':25,'address':'Uptown','phone':9876543210}
+print(my_dict)
+print(my_dict_2)
