12b1629bc7e5a1292cffef02f0a62b9e23b5580b
HowellHao <howellhao.work@gmail.com>
2024-12-27 16:43:45 -0700

    add file to Python

--- Python/Dictionary/Create_Dict.py
+++ Python/Dictionary/Create_Dict.py
@@ -1,8 +1,17 @@
-my_dict={}
-my_dict['name']='Jack'
-my_dict['age']=26
-my_dict['address']='Downtown'
-my_dict['phone']=1234567890
-my_dict_2={'name':'Jill','age':25,'address':'Uptown','phone':9876543210}
-print(my_dict)
-print(my_dict_2)
+my_dict = {}
+my_dict_2 = {}
+while True:
+    try:
+        my_dict['name'] = input("Enter name to dict: ")
+        my_dict['age'] = int(input("Enter age to dict: "))
+        my_dict['address'] = input("Enter address to dict: ")
+        my_dict['phone'] = int(input("Enter phone number to dict: "))
+        my_dict_2['name'] = input("Enter name to dict 2: ")
+        my_dict_2['age'] = int(input("Enter age to dict 2: "))
+        my_dict_2['address'] = input("Enter address to dict 2: ")
+        my_dict_2['phone'] = int(input("Enter phone number to dict 2: "))
+        print(my_dict)
+        print(my_dict_2)
+        break
+    except ValueError:
+        print("Please enter a valid input.")
